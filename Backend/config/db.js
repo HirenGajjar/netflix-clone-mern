353af90a212b0822e48f29f86432f8edc75857e8
HirenGajjar <gajjarhiren111@gmail.com>
2024-08-28 14:46:31 -0700

    Add database connection

---
+++ Backend/config/db.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import { ENV_VARIABLE } from "./envVars.js";
+//
+export const connectDB = async () => {
+  try {
+    mongoose.connect(`${ENV_VARIABLE.MONGO_URI}`);
+  } catch (error) {
+    process.exit(1);
+  }
+};
--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import authRoutes from "./routes/auth.routes.js";
-
+import { connectDB } from "./config/db.js";
+import { ENV_VARIABLE } from "./config/envVars.js";
 //
 const app = express();
-
+const PORT = ENV_VARIABLE.PORT;
 //
 app.use("/api/v1/auth", authRoutes);
-app.listen(3000);
+app.listen(PORT, () => {
+  connectDB();
+});
